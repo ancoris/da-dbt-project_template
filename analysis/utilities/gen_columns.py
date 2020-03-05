8bfe10b15ee5b3c462a331a71dbd16d7064bb104
Thomas Langton <61457328+tlangton2@users.noreply.github.com>
2020-03-05 10:44:10 +0000

    Script to extract BQ dataset tables columns in schema.yml friendly way

---
+++ analysis/utilities/gen_columns.py
@@ -0,0 +1,131 @@
+# Python 3.7
+# Written by: TL - if it breaks let me know
+import json
+import os
+import time
+
+# Controls console output
+output = True
+
+startTime = time.time()
+
+# This script assumes no dots (.) in dataset name or table name.
+
+# Specify project id
+projectId = "data-sandbox-266217"
+
+
+# list target bq source datasets
+targets = ["london_santander"]
+
+#
+placedParentDir, filename = os.path.split(__file__)
+scriptFolder = placedParentDir + "/"
+
+
+# stores sh scripts for listing tables in a dataset
+tableSh = "tables"
+
+# stores sh scripts for generating table schemas in a dataset
+tablecolSh = "tableCols"
+
+# .sh scripts stored here
+shlocation = "dump/shDump/"
+
+# table lists for each dataset stored here
+tableDump = "dump/tableDump/"
+
+
+# schema .jsons stored here
+jsonDump = "dump/jsonDump/"
+
+# schema.yml compatible table columns stored here
+columnDump = "columns/"
+
+# Creates directories given the above
+if not os.path.exists(os.path.dirname(scriptFolder+shlocation)):
+    os.makedirs(os.path.dirname(scriptFolder+shlocation), exist_ok=True)
+
+if not os.path.exists(os.path.dirname(scriptFolder+jsonDump)):
+    os.makedirs(os.path.dirname(scriptFolder+jsonDump), exist_ok=True)
+
+if not os.path.exists(os.path.dirname(scriptFolder+tableDump)):
+    os.makedirs(os.path.dirname(scriptFolder+tableDump), exist_ok=True)
+
+if not os.path.exists(os.path.dirname(scriptFolder+columnDump)):
+    os.makedirs(os.path.dirname(scriptFolder+columnDump), exist_ok=True)
+
+for targetSource in targets:
+    #####
+    with open("{}{}{}_{}.sh".format(scriptFolder, shlocation, tableSh, targetSource), "w") as file:
+        file.write("bq ls {}:{} | awk '{{print $1}}' | tail +3  > {}{}tableList_{}.txt".format(projectId,
+                                                                                               targetSource, scriptFolder, tableDump, targetSource))
+
+    os.system("sh {}{}{}_{}.sh".format(
+        scriptFolder, shlocation, tableSh, targetSource))
+
+    with open("{}{}tableList_{}.txt".format(scriptFolder, tableDump, targetSource), "r") as tables:
+        if output:
+            print("Extracting tables from dataset: {}".format(targetSource))
+        tableList = {}  # this isn't necessary as datasets are looped over
+        tableList[targetSource] = []
+        for table in tables:
+            tableList[targetSource].append(table.rstrip())
+
+    ######
+
+    with open("{}{}{}_{}.sh".format(scriptFolder, shlocation, tablecolSh, targetSource), "w") as f:
+        for dataset in tableList.keys():
+            for table in tableList[dataset]:
+                if output:
+                    print("Extracting columns from: {}.{}".format(
+                        targetSource, table))
+                f.write("bq show --schema {}:{}.{} > {}{}{}.{}.json\n".format(projectId, dataset,
+                                                                              table, scriptFolder, jsonDump, dataset, table))
+
+    os.system("sh {}{}{}_{}.sh".format(scriptFolder,
+                                       shlocation, tablecolSh, targetSource))
+    if output:
+        print("-"*25)
+#
+#
+#
+#  Writes output
+datasetInfo = {}
+for file in os.listdir(scriptFolder+jsonDump):
+    if file.split(".")[0] not in datasetInfo.keys():
+        datasetInfo[file.split(".")[0]] = {"tableCount": 0, "columnCount": 0}
+    datasetInfo[file.split(".")[0]]["tableCount"] += 1
+    with open(scriptFolder+jsonDump+"/"+file, "r") as f:
+        data = json.load(f)
+        with open("{}{}{}".format(scriptFolder, columnDump, os.path.splitext(file)[0]+".yml"), 'w') as columnTxt:
+            if output:
+                print("Generating yml for {}:{}".format(
+                    file.split(".")[0], file.split(".")[1]))
+            columnTxt.write("sources:\n")
+            columnTxt.write("  - name: {}\n".format(file.split(".")[0]))
+            columnTxt.write("    tables:\n")
+            columnTxt.write("      - name: {}\n".format(file.split(".")[1]))
+            columnTxt.write("        columns:\n")
+            for dict in data:
+                columnTxt.write("          - name: {}\n".format(dict["name"]))
+
+                datasetInfo[file.split(".")[0]]["columnCount"] += 1
+
+#
+#
+#
+# Exit console output
+executionTime = time.time() - startTime
+if output:
+    print("-"*25)
+    print("Script execution time: {:.2f} seconds".format(executionTime))
+    print("Extracted from {} bq datasets".format(len(datasetInfo)))
+    print("-"*25)
+    for dataset in datasetInfo.keys():
+        print("Dataset name: {}".format(dataset))
+        print("Number of tables: {}".format(
+            datasetInfo[dataset]["tableCount"]))
+        print("Number of columns: {}".format(
+            datasetInfo[dataset]["columnCount"]))
+        print("-"*25)
