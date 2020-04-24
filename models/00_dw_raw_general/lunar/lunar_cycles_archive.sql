eb7ed0e4cbe6590779db5806f8cfba3bf2a54d83
James Green <52244147+james-the-first@users.noreply.github.com>
2020-04-24 18:14:46 +0100

    Lunar

---
+++ models/00_dw_raw_general/lunar/lunar_cycles_archive.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized='archive_incremental',
+        schema='raw_dw_general',
+        partition_by='date(meta_process_time)'
+    )
+}}
+select  full_moon_date,
+        is_total_eclipse,
+        is_partial_eclipse,
+        region,
+        meta_delivery_time,
+        {{ dbt_macros.meta_process_time() }}    as meta_process_time
+from    {{ref('lunar_cycles_dbt_seed')}}
