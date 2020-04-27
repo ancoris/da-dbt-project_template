6da4bbd61742294f5316cf2f2897548f7b3885c7
James Green <52244147+james-the-first@users.noreply.github.com>
2020-04-27 12:04:07 +0100

    Copy models from Lush project

---
+++ models/00_dw_raw/bq-public-data.london_cycle_hire/cycle_stations_archive.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='archive_incremental',
+        schema='raw_cycle_hire_archive',
+        partition_by='date(meta_process_time)'
+    )
+}}
+select  id,
+        installed,
+        latitude,
+        locked,
+        longitude,
+        name,
+        bikes_count,
+        docks_count,
+        nbEmptyDocks,
+        temporary,
+        terminal_name,
+        install_date,
+        removal_date,
+
+        -- meta fields
+        {{dbt_macros.meta_process_time() }} as meta_process_time,
+        meta_delivery_time
+from {{ ref('cycle_stations_raw') }}
+
+-- we only load what has been delivered in this process window to simulate an incremental load
+where meta_delivery_time = {{dbt_macros.meta_process_time() }}
---
+++ models/00_dw_raw/dw_raw_general/lunar/lunar_cycles_clean.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized='view',
+        schema='raw_general_clean'
+    )
+}}
+select  full_moon_date,
+        is_total_eclipse,
+        is_partial_eclipse,
+        region,
+        meta_delivery_time,
+        meta_process_time
+from    {{ref('lunar_cycles_archive')}}
+where meta_process_time = {{dbt_macros.meta_process_time() }}
