0250615dd57e0db04666283b2547883be6af8f84
bsharp-ledger <bsharp-ledger@ancoris.com>
2021-01-13 09:38:50 +0000

    Change clean_numeric and clean_int64 macros to alllow negatives

--- macros/dbt_macros/clean_numeric.sql
+++ macros/dbt_macros/clean_numeric.sql
@@ -1,3 +1,3 @@
 {% macro clean_numeric(string) %}
-    safe_cast(regexp_replace(ifnull({{string}},'0'), r'[^0-9.]', '') as numeric)
+    safe_cast(regexp_replace(ifnull({{string}},'0'), r'[^0-9.-]', '') as numeric)
 {% endmacro %}
