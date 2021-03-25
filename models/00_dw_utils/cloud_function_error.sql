f6cc6851e33d99fdb7790354fb3c30dc02d85511
bailey <bsharp-ledger@ancoris.com>
2021-03-25 14:34:08 +0000

    small tweak to cloud function error model

--- models/00_dw_utils/cloud_function_error.sql
+++ models/00_dw_utils/cloud_function_error.sql
@@ -1,10 +1,11 @@
 {{
     config(
         materialized='materialization_none',
+        schema='force_to_dw_utils'
     )
 }}
 
 -- This can be created elsewhere, kept within dbt for simplicity
 -- All inserts handled by cloud function
 
-create table if not exists `{{ this.database }}`.`dw_utils`.`cloud_function_error` (cloud_function STRING, error_time STRING, error STRING)
+create table if not exists {{ this.database }}.{{this.schema}}.{{this.name}} (cloud_function STRING, error_time STRING, error STRING)
